55ce259a018d4cd5d5dc720ec07ceee251c67b57
niyonabil <boguiman@gmail.com>
2025-12-03 15:44:49 +0100

    Fix: Export VersionCheckResponse interface

--- backend/src/version/version.service.ts
+++ backend/src/version/version.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma.service';
 
-interface VersionCheckResponse {
+export interface VersionCheckResponse {
     updateAvailable: boolean;
     isForced: boolean;
     latestVersion?: string;
